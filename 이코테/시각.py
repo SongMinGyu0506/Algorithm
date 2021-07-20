df0e660cef555ed2528f444a76f16cb24aa41d77
SongMinGyu0506 <ggp04114@naver.com>
2021-07-20 22:36:00 +0900

    구현 추가

---
+++ 이코테/시각.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+counter = 0
+for i in range(n+1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i)+str(j)+str(k):
+                counter += 1
+
+print(counter)
