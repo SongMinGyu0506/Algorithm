0eb71a966746d65419a3bd5d807f1ec3edfccf49
Song_MinGyu <40915234+SongMinGyu0506@users.noreply.github.com>
2021-08-26 16:59:44 +0900

    연습문제

---
+++ Programmers/Lv2/최솟값 만들기.py
@@ -0,0 +1,11 @@
+def solution(A,B):
+    answer = 0
+    answerList = []
+    
+    A.sort()
+    B.sort(reverse=True)
+    
+    for i in range(len(A)):
+        answer = answer + (A[i]*B[i])
+        
+    return answer
