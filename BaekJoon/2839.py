7fedb45140a5fcac866553966e8b7e6f0c9957b0
SongMinGyu0506 <ggp04114@naver.com>
2022-01-16 21:22:00 +0900

    backjoon 2839 Fail

--- BaekJoon/2839.py
+++ BaekJoon/2839.py
@@ -1,21 +1,11 @@
 N = int(input())
-
-result = 0
-three_counter = 0
-five_counter = 0
-if N < 5:
-    three_counter = N//3
-    if N%3 != 0:
-        three_counter = 0
-        result = -1
+dn = [float("inf")]*5001
+if len(dn) < 6:
+    print(-1)
 else:
-    five_counter = N//5
-    if N%5 != 0:
-        three_counter += (N%5)//3
-        if (N%5)%3 != 0:
-            three_counter = 0
-            five_counter = 0
-            result = -1
-
-print(result+five_counter+three_counter)
-    
+    dn[3] = 1
+    dn[5] = 1
+    for i in range(6,N+1):
+        dn[i] = min(dn[i-3]+1,dn[i-5]+1)
+    print(dn[0:N+1])
+    print(dn[N])
