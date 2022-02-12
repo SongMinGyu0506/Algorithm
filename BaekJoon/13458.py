34055ff479d23fb248c9f270b5173425b8da42b6
songmingyu <ggp04114@naver.com>
2022-02-12 20:59:09 +0900

    BOJ - Bronze 2 Math(Samsung)

--- BaekJoon/13458.py
+++ BaekJoon/13458.py
@@ -1,15 +1,22 @@
-N = int(input())
-A = list(map(int,input().split(' ')))
-B,C = map(int,input().split(' '))
+import sys
+
+
+N = int(sys.stdin.readline())
+tester = list(map(int,sys.stdin.readline().split()))
+B,C = map(int,sys.stdin.readline().split())
 
 result = N
-A_tmp = [i-B for i in A]
-print(A_tmp)
-
-for i in A_tmp:
-    if i%C != 0:
-        result = result + (i//C)+1
+for i in range(len(tester)):
+    if tester[i] - B <= 0:
+        tester[i] = 0
     else:
-        result = result + (i//C)
+        tester[i] = tester[i] - B
 
-print (result)
+for i in range(len(tester)):
+    result = result + (tester[i]//C)
+    if tester[i]//C >= 1 and tester[i]%C != 0:
+        tester[i] = tester[i]%C
+    if tester[i]//C == 0 and tester[i]%C != 0:
+        result += 1
+
+print(result)
